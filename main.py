0867b91aed295ae336b26ff64a8f8bb54cc4b5d2
GiacomoPugliese <puglieseg24@gmail.com>
2024-05-09 01:18:13 -0400

    back

--- main.py
+++ main.py
@@ -59,7 +59,25 @@ def startup_event():
 
 @app.get("/auth")
 def auth(user_id: str):
-    return {"message": "Auth endpoint is working!", "user_id": user_id}
+    # Create the Flow instance
+    flow = Flow.from_client_secrets_file(
+        'credentials.json',
+        scopes=['https://www.googleapis.com/auth/drive'],
+        redirect_uri='https://leadership-initiatives-0c372bea22f2.herokuapp.com/callback' 
+    )
+
+    # Set the state for CSRF protection#
+    flow.state = secrets.token_hex(16)
+
+    # Get the authorization URL for the consent screen
+    authorization_url, _ = flow.authorization_url(prompt='consent', access_type='offline')
+
+    # Save the user_id and Flow instance for later
+    tokens[0] = {"user_id": user_id, "flow": flow}
+    print(f"Auth endpoint: State is {flow.state}")
+    print(f"Auth endpoint: Tokens are {tokens}")
+    # Return the authorization URL in the response
+    return {"authorization_url": authorization_url}
 
 
 @app.get("/callback")
